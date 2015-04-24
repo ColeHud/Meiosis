af748d6dc03005f661667a1009a1495f2f47ee2e
ColeHud <cmhudson11@gmail.com>
2015-04-23 21:25:17 -0400

    Game and learn home screens

---
+++ Meiosis/GameHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  GameHomeViewController.h
+//  Meiosis
+//
+//  Created by Cole on 4/23/15.
+//  Copyright (c) 2015 Cole Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GameHomeViewController : UIViewController
+
+@end
---
+++ Meiosis/LearnHomeViewController.m
@@ -0,0 +1,39 @@
+//
+//  LearnHomeViewController.m
+//  Meiosis
+//
+//  Created by Cole on 4/23/15.
+//  Copyright (c) 2015 Cole Hudson. All rights reserved.
+//
+
+#import "LearnHomeViewController.h"
+
+@interface LearnHomeViewController ()
+
+@end
+
+@implementation LearnHomeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
